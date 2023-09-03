a6566449a29a3222a69a69e73b1e6898954f2f92
Christian W. Feldmann <bob@helium>
2023-09-03 16:45:26 +0200

    add/rewrite tests

---
+++ tests/test_fingerprints.py
@@ -0,0 +1,64 @@
+import unittest
+
+import os
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from rdkit import Chem
+from scipy import sparse
+
+from compchemkit.fingerprints import (
+    UnfoldedMorganFingerprint,
+    FragmentFingerprint,
+)
+from compchemkit.utils.molecule_validity import construct_check_mol_list
+
+test_folder = os.path.dirname(__file__)
+smiles_df = pd.read_csv(f"{test_folder}/test_data/test_smiles.tsv")
+smiles_list = smiles_df["SMILES"].to_list()
+
+
+class ConstructingFingerprints(unittest.TestCase):
+    def test_independence_of_constructing(self) -> None:
+        mol_obj_list = construct_check_mol_list(smiles_list)
+        ecfp2_1 = UnfoldedMorganFingerprint()
+        fp1 = ecfp2_1.fit_transform(mol_obj_list)
+        ecfp2_2 = UnfoldedMorganFingerprint()
+        ecfp2_2.fit(mol_obj_list)
+        fp2 = ecfp2_2.transform(mol_obj_list)
+        self.assertTrue((fp1 != fp2).nnz == 0)
+
+    def test_independence_of_constructing_parallel(self) -> None:
+        mol_obj_list = construct_check_mol_list(smiles_list)
+        # Fingerprint 1
+        ecfp2_1 = UnfoldedMorganFingerprint(n_jobs=2)
+        fp1 = ecfp2_1.fit_transform(mol_obj_list)
+        # Fingerprint 2
+        ecfp2_2 = UnfoldedMorganFingerprint(n_jobs=2)
+        ecfp2_2.fit(mol_obj_list)
+        fp2 = ecfp2_2.transform(mol_obj_list)
+        # Fingerprint 3
+        ecfp2_3 = UnfoldedMorganFingerprint(n_jobs=1)
+        fp3 = ecfp2_3.fit_transform(mol_obj_list)
+        # Compare for equal
+        self.assertEqual((fp1 != fp2).nnz, 0)
+        self.assertEqual((fp1 != fp3).nnz, 0)
+
+    def test_substructure_fp(self) -> None:
+        smarts_list = ["[#6]", "[#7]", "[#8]"]
+        frag_fingerprint = FragmentFingerprint(smarts_list)
+        fp = frag_fingerprint.transform(construct_check_mol_list(smiles_list))
+        expected_fp: npt.NDArray[np.int_] = np.zeros((len(smiles_list), len(smarts_list)), dtype=int)
+        for i, smart in enumerate(smarts_list):
+            smart_obj = Chem.MolFromSmarts(smart)
+            for j, smi in enumerate(smiles_list):
+                mol = Chem.MolFromSmiles(smi)
+                if mol.HasSubstructMatch(smart_obj):
+                    expected_fp[j, i] = 1
+        expected_fp_sparse = sparse.csr_matrix(expected_fp)
+        self.assertEqual((fp != expected_fp_sparse).nnz, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -1,38 +1,25 @@
+import os
 import unittest
 
 import numpy as np
+import pandas as pd
 import scipy.sparse as sparse
 
 from compchemkit.fingerprints import UnfoldedMorganFingerprint
 from compchemkit.kernel import similarity_from_dense, similarity_from_sparse
-from compchemkit.supporting_functions import construct_check_mol_list
-
-# noinspection SpellCheckingInspection
-smiles_list = [
-    "c1ccccc1",
-    "CC(=O)C1CCC2C1(CCC3C2CCC4=CC(=O)CCC34C)C",
-    "c1cc(ccc1C2CCNCC2COc3ccc4c(c3)OCO4)F",
-    "c1c(c2c(ncnc2n1C3C(C(C(O3)CO)O)O)N)C(=O)N",
-    "Cc1cccc(c1NC(=O)c2cnc(s2)Nc3cc(nc(n3)C)N4CCN(CC4)CCO)Cl",
-    "CN(C)c1c2c(ncn1)n(cn2)C3C(C(C(O3)CO)NC(=O)C(Cc4ccc(cc4)OC)N)O",
-    "CC12CCC(CC1CCC3C2CC(C4(C3(CCC4C5=CC(=O)OC5)O)C)O)O",
-]
+from compchemkit.utils.molecule_validity import construct_check_mol_list
 
 
-class ConstructingFingerprints(unittest.TestCase):
-    def test_independence_of_constructing(self) -> None:
-        mol_obj_list = construct_check_mol_list(smiles_list)
-        ecfp2_1 = UnfoldedMorganFingerprint()
-        fp1 = ecfp2_1.fit_transform(mol_obj_list)
-        ecfp2_2 = UnfoldedMorganFingerprint()
-        ecfp2_2.fit(mol_obj_list)
-        fp2 = ecfp2_2.transform(mol_obj_list)
-        self.assertTrue((fp1 != fp2).nnz == 0)
+test_folder = os.path.dirname(__file__)
+smiles_df = pd.read_csv(f"{test_folder}/test_data/test_smiles.tsv")
+smiles_list = smiles_df["SMILES"].to_list()
 
 
 class Kernel(unittest.TestCase):
     def test_sparse_kernel_simple_vectors(self) -> None:
-        test_fingerprint1 = sparse.csr_matrix(np.array([[0, 0, 0, 1], [0, 0, 1, 1], [0, 1, 0, 0]]))
+        test_fingerprint1 = sparse.csr_matrix(
+            np.array([[0, 0, 0, 1], [0, 0, 1, 1], [0, 1, 0, 0]])
+        )
         test_fingerprint2 = sparse.csr_matrix(
             np.array(
                 [
@@ -82,8 +69,10 @@ class Kernel(unittest.TestCase):
         sim_matrix = similarity_from_sparse(fp1, fp1)
         self.assertEqual(sim_matrix.shape[0], sim_matrix.shape[1])
         self.assertEqual(sim_matrix.shape[0], len(mol_obj_list))
-        self.assertTrue(np.all(np.isclose(sim_matrix.diagonal(), np.ones((len(mol_obj_list), 1)))))
+        self.assertTrue(
+            np.all(np.isclose(sim_matrix.diagonal(), np.ones((len(mol_obj_list), 1))))
+        )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
