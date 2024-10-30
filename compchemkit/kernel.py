52812373b38e470274b403f54d472ef693c2e26c
Christian W. Feldmann <christian.w.feldmann@gmail.com>
2024-10-30 22:09:17 +0100

    remove __main__ function

--- compchemkit/kernel.py
+++ compchemkit/kernel.py
@@ -59,31 +59,3 @@ def tanimoto_from_sparse(
     union = norm_1 + norm_2.T - intersection
     similarity: npt.NDArray[np.float_] = intersection / union
     return similarity
-
-
-if __name__ == "__main__":
-    fp1 = sparse.csr_matrix(
-        np.array(
-            [
-                [0, 0, 0, 1],
-                [0, 0, 1, 1],
-                [0, 1, 0, 0],
-            ]
-        )
-    )
-    fp2 = sparse.csr_matrix(
-        np.array(
-            [
-                [0, 0, 0, 1],
-                [0, 0, 1, 1],
-                [0, 1, 1, 0],
-                [1, 0, 0, 0],
-                [1, 0, 0, 0],
-            ]
-        )
-    )
-    sim = tanimoto_from_sparse(fp1, fp2)
-    print(type(sim))
-    print(isinstance(sim, np.ndarray))
-    print(sim.shape)
-    print(sim)
